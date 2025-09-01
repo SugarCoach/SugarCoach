5068e18ca56a1cda4c03c49b85de31056b1ebcd9
Isa-Rocket <berizzoalejandraisabel@gmail.com>
2025-08-31 21:35:57 -0300

    chore(git): add .gitignore y dejar de trackear builds/IDE/keys

--- app/build/tmp/kapt3/stubs/debug/com/sugarcoachpremium/ui/onboarding/interactor/OnBoardingInteractorImp.java
+++
@@ -1,5 +0,0 @@
-package com.sugarcoachpremium.ui.onboarding.interactor;
-
-@kotlin.Metadata(mv = {2, 2, 0}, k = 1, xi = 48, d1 = {"\u0000\n\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0000\bf\u0018\u00002\u00020\u0001\u00a8\u0006\u0002\u00c0\u0006\u0003"}, d2 = {"Lcom/sugarcoachpremium/ui/onboarding/interactor/OnBoardingInteractorImp;", "Lcom/sugarcoachpremium/data/ui/base/interactor/Interactor;", "app_debug"})
-public abstract interface OnBoardingInteractorImp extends com.sugarcoachpremium.data.ui.base.interactor.Interactor {
-}
